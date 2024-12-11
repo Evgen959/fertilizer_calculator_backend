e0fd7674f1de4d0392ae16e03cbc392150ab0808
Evgen <evgen.tm959@gmail.com>
2024-12-11 10:22:37 +0100

    Start-project

---
+++ src/main/java/org/example/FertilizerCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FertilizerCalculatorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FertilizerCalculatorBackendApplication.class, args);
+    }
+
+}
